8fa328f1a82af628504a6474e5ce0ae188dbdf3e
Nisum <Nisum@DESKTOP-HKHVLGO>
2017-12-01 09:56:52 +0530

    First Commit

---
+++ src/com/java/CustomerService.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public interface CustomerService {
+
+	public boolean addCustomer(Customer c);
+	
+	public boolean removeCustomer(int id);
+	
+	public Customer getCustomer(int id);
+	
+	public Customer[] getAllCustomer();
+}
---
+++ src/com/java/CustomerServiceImpl.java
@@ -0,0 +1,51 @@
+package com.java;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CustomerServiceImpl implements CustomerService{
+
+	private static Map<Integer,Customer> customer = new HashMap<Integer,Customer>();
+	
+	@Override
+	public boolean addCustomer(Customer c) {
+		if(customer.get(c.getId()) != null) {
+			return false;
+		}
+		customer.put(c.getId(), c);
+		System.out.println("Object Added Successfully !!");
+		return true;
+	}
+
+	@Override
+	public boolean removeCustomer(int id) {
+
+		if(customer.get(id) == null) {
+			return false;
+		}
+		customer.remove(id);
+		return true;
+	}
+
+	@Override
+	public Customer getCustomer(int id) {
+		
+		return customer.get(id);
+	}
+
+	@Override
+	public Customer[] getAllCustomer() {
+		
+		Set<Integer> ids = customer.keySet();
+		Customer[] c = new Customer[ids.size()];
+		int i=0;
+		for(Integer id : ids) {
+			c[i] = customer.get(id);
+			i++;
+		}
+		return c;
+	}
+
+	
+}
